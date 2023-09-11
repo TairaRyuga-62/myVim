08a544bab1a0ad97b08be18eaeb0d9f223609d5f
TairaRyuga-62 <62@a440.technology>
2023-09-11 15:31:21 +0900

    added plugin copilot.

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,8 @@
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+return
+{
+  "github/copilot.vim",
+  lazy=false,
+}
